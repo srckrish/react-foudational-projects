e76a9cd84eca86406341e24333f23f81c532b308
srckrish <krish.dahal82@gmail.com>
2026-03-28 15:13:38 +0545

    add Multi Selection feature

--- 01_accordion/src/App.jsx
+++ 01_accordion/src/App.jsx
@@ -1,33 +1,75 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import data from "./data";
 function App() {
-  const [selected, setSelected] = useState(null);
-  const handleSingleSelection = (getCurrentItemId) => {
-    setSelected(getCurrentItemId === selected ? null : getCurrentItemId);
+  const [singleSelection, setSingleSelection] = useState(null);
+  const handleSingleSelection = (getCurrentId) => {
+    setSingleSelection((prev) => (prev !== getCurrentId ? getCurrentId : null));
   };
 
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiSelection, setMultiSelection] = useState([]);
+  const handleMultiSelection = (getCurrentId) => {
+    setMultiSelection((prev) => {
+      let cpy = [...prev];
+      const index = cpy.indexOf(getCurrentId);
+      if (index === -1) cpy.push(getCurrentId);
+      else cpy.splice(index, 1);
+
+      return cpy;
+    });
+  };
   return (
     <>
       {data && data.length > 0 ? (
-        <div className="wrapper mx-auto max-w-2xl mt-10">
-          <div className="container shadow-cyan-200 shadow-xl rounded-2xl border border-gray-400">
-            {data.map((dataItem) => {
-              return (
-                <div className="itemList px-5">
-                  <div className="border-b border-gray-300">
-                  <div className="item py-5 flex justify-between items-center" onClick={()=> handleSingleSelection(dataItem.id)}>
-                    <h2 className="title text-lg font-medium text-gray-800">{dataItem.question}</h2>
-                    <span className= {`icon text-2xl ${selected === dataItem.id ? `rotate-45`:`rotate-0`}`}>+</span>
+        <div className="wrapper mx-auto max-w-2xl my-10 px-4">
+          <div className="container shadow-cyan-200 shadow-xl rounded-2xl border border-gray-400 overflow-hidden">
+            <div className="px-4 py-3 bg-[#fbfbfc] text-sm font-medium border-b border-gray-300 flex justify-between items-center">
+              <p>{data.length} results </p>
+              <button
+                className="hover:text-blue-400 text-blue-700"
+                onClick={() => {
+                  setEnableMultiSelection((prev) => !prev);
+                  setMultiSelection([]);
+                  setSingleSelection(null);
+                }}
+              >
+                {`${enableMultiSelection ? `Enable Single Selection` : `Enable Multi Selection`}`}
+              </button>
+            </div>
+            <div className="itemList px-5">
+              {data.map((dataItem) => {
+                return (
+                  <div
+                    key={dataItem.id}
+                    className="item border-b border-gray-300"
+                  >
+                    <button
+                      className="item py-5 flex justify-between items-center w-full gap-1"
+                      onClick={() =>
+                        enableMultiSelection
+                          ? handleMultiSelection(dataItem.id)
+                          : handleSingleSelection(dataItem.id)
+                      }
+                    >
+                      <span className="title md:text-lg text-md text-left font-medium text-gray-800">
+                        {dataItem.question}
+                      </span>
+                      <span
+                        className={`icon text-2xl transition-all duration-300 ${singleSelection === dataItem.id || multiSelection.includes(dataItem.id) ? `rotate-45` : `rotate-0`}`}
+                      >
+                        +
+                      </span>
+                    </button>
+                    {singleSelection === dataItem.id ||
+                    multiSelection.includes(dataItem.id) ? (
+                      <p className="content pb-5 text-gray-600 transition-all duration-300 ease-in-out">
+                        {dataItem.answer}
+                      </p>
+                    ) : null}
                   </div>
-                  {(selected === dataItem.id) &&(
-                  <h3 className="content pb-5 text-gray-600">
-                    {dataItem.answer}
-                  </h3>
-                  )}
-                  </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
         </div>
       ) : (
