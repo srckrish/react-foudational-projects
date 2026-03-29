99d0f43a9ae923f718bc82dbc1c0fc2bf45a6a72
srckrish <krish.dahal82@gmail.com>
2026-03-29 19:28:18 +0545

    improve ui and add generated color code

--- 02_random_color_generator/src/App.jsx
+++ 02_random_color_generator/src/App.jsx
@@ -36,29 +36,40 @@ function App() {
 
   return (
     <div
-      className="h-screen w-full overflow-hidden relative"
+      className="h-screen w-full overflow-hidden relative transition-all duration-500"
       style={{ backgroundColor: color }}
     >
-      <div className="flex justify-center items-end h-full gap-4 py-5">
-        <div className="bg-white text-green-800 rounded-full text-2xl font-medium flex justify-around items-center gap-5 px-5 py-2">
-          <button
-            className={`rounded-full px-5 py-2 ${typeOfColor === "hex" ? "bg-green-500 text-white" : "hover:bg-green-500 hover:text-white"}`}
-            onClick={() => setTypeOfColor("hex")}
-          >
-            Hex Color
-          </button>
-          <button
-            className={`rounded-full px-5 py-2 ${typeOfColor === "rgb" ? "bg-green-500 text-white" : "hover:bg-green-500 hover:text-white"}`}
-            onClick={() => setTypeOfColor("rgb")}
-          >
-            RGB Color
-          </button>
-          <button
-            className="rounded-full px-5 py-2 hover:bg-blue-500 hover:text-white cursor-pointer"
-            onClick={() => handleColorType()}
-          >
-            Generate
-          </button>
+      <div className="flex justify-center items-end h-full py-5">
+        <div className="flex justify-center flex-col items-center flex-wrap gap-7 mx-auto max-w-2xl">
+          <div className="bg-white text-green-800 rounded-full text-sm sm:text-xl font-medium px-5 py-2">
+            {color}
+          </div>
+          <div className="bg-white text-green-800 rounded-full text-sm sm:text-xl font-medium flex flex-wrap justify-around items-center gap-3 sm:gap-5 px-4 py-2">
+            <button
+              className={`rounded-full px-5 py-2 ${typeOfColor === "hex" ? "bg-green-500 text-white" : ""}`}
+              onClick={() => {
+                setTypeOfColor("hex");
+                setColor("#000000");
+              }}
+            >
+              Hex Color
+            </button>
+            <button
+              className={`rounded-full px-5 py-2 ${typeOfColor === "rgb" ? "bg-green-500 text-white" : ""}`}
+              onClick={() => {
+                setTypeOfColor("rgb");
+                setColor("rgb(0, 0, 0)");
+              }}
+            >
+              RGB Color
+            </button>
+            <button
+              className="rounded-full px-5 py-2 hover:bg-blue-500 hover:text-white cursor-pointer"
+              onClick={() => handleColorType()}
+            >
+              Generate
+            </button>
+          </div>
         </div>
       </div>
     </div>
