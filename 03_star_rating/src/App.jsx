db4e5f1ddfe21b436ea9c5cf6d8464adf529e94c
srckrish <krish.dahal82@gmail.com>
2026-03-31 23:08:28 +0545

    add star hover and click functionality

--- 03_star_rating/src/App.jsx
+++ 03_star_rating/src/App.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
 
-function App() {
-  return <div>App</div>;
+function App({stars = 5}) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(getCurrentIndex) {
+    setRating(getCurrentIndex);
+  }
+
+  function handleMouseEnter(getCurrentIndex) {
+    setHover(getCurrentIndex);
+  }
+
+  function handleMouseLeave() {
+    setHover(rating);
+  }
+
+  return (
+    <>
+      <div className="star-rating min-h-screen w-full flex">
+        {[...Array(stars)].map((_, index) => {
+          index += 1;
+          console.log("index: ", index);
+          console.log("hover: ", hover);
+          console.log("rating: ", rating);
+          
+          return (
+            <FaStar
+              key={index}
+              className={`${index <= (hover || rating) ? "text-[#898500]" : ""}`}
+              onClick={() => handleClick(index)}
+              onMouseMove={() => handleMouseEnter(index)}
+              onMouseLeave={() => handleMouseLeave()}
+              size={40}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
