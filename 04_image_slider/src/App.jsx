62f531bcbf235055fb618aa77c69272fa3358cde
srckrish <krish.dahal82@gmail.com>
2026-04-01 13:22:53 +0545

    Get the images data from API

--- 04_image_slider/src/App.jsx
+++ 04_image_slider/src/App.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div>App</div>
-  )
+function App({ url, page = 1, limit = 5 }) {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchImages(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `${getUrl}?page=${page}&limit=${limit}`,
+      );
+      const data = await response.json();
+
+      if (data) {
+        setImages(data);
+        setLoading(false);
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  }, [url]);
+
+  console.log(images);
+
+  if (loading) {
+    return <div>Loading data...</div>;
+  }
+
+  if (errorMessage !== null) {
+    return <div> Error Occured! {errorMessage} </div>;
+  }
+
+  return <div>App</div>;
 }
 
-export default App
+export default App;
