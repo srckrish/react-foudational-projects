c1a1faefa7b06871e0c90870aba1ef4f5e1901c2
srckrish <krish.dahal82@gmail.com>
2026-04-03 23:22:00 +0545

    Get data from API

--- 05_load_more/src/App.jsx
+++ 05_load_more/src/App.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return <div className="wrapper">App</div>;
+function App({ url, limit, skip }) {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [count, setCount] = useState(0);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  async function fetchProducts(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `${getUrl}?limit=${limit}&skip=${count === 0 ? 0 : count * 20}`,
+      );
+      const data = await response.json();
+
+      if (data) {
+        setProducts(data);
+        setLoading(false);
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (url !== "") fetchProducts(url);
+  }, [url]);
+
+  console.log(products);
+
+  if (loading) {
+    return <div>Loading data...</div>;
+  }
+
+  if (errorMessage !== null) {
+    return <div>Error Occured! {errorMessage} </div>;
+  }
+
+  return (
+    <div className="wrapper">
+      {products && products.length
+        ? products.map((productItem) => {
+            //
+          })
+        : null}
+      <div className="container"></div>
+    </div>
+  );
 }
 
 export default App;
