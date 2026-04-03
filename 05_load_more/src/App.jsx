4f4ebdd17036b144dd16ffd79dfab1692e044ed1
srckrish <krish.dahal82@gmail.com>
2026-04-03 23:59:39 +0545

    Display products

--- 05_load_more/src/App.jsx
+++ 05_load_more/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-function App({ url, limit, skip }) {
+function App({ url }) {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
@@ -10,25 +10,29 @@ function App({ url, limit, skip }) {
     try {
       setLoading(true);
       const response = await fetch(
-        `${getUrl}?limit=${limit}&skip=${count === 0 ? 0 : count * 20}`,
+        `${getUrl}?limit=20&skip=${count === 0 ? 0 : count * 20}`,
       );
       const data = await response.json();
 
-      if (data) {
-        setProducts(data);
+      if (data && data.products && data.products.length) {
+        setProducts(data.products);
         setLoading(false);
       }
-    } catch (error) {
-      setErrorMessage(error.message);
+    } catch (e) {
+      setErrorMessage(e.message);
       setLoading(false);
     }
   }
 
   useEffect(() => {
     if (url !== "") fetchProducts(url);
-  }, [url]);
+  }, [url, count]);
 
   console.log(products);
+
+  useEffect(() => {
+    if (products && products.length === 100) setIsButtonDisabled(true);
+  }, [products]);
 
   if (loading) {
     return <div>Loading data...</div>;
@@ -39,13 +43,35 @@ function App({ url, limit, skip }) {
   }
 
   return (
-    <div className="wrapper">
-      {products && products.length
-        ? products.map((productItem) => {
-            //
-          })
-        : null}
-      <div className="container"></div>
+    <div className="wrapper mx-auto max-w-7xl mt-10">
+      <div className="container grid grid-cols-5">
+        {products && products.length
+          ? products.map((productItem) => {
+              return (
+                <div
+                  className="product border border-gray-400 flex items-center flex-col"
+                  key={productItem.id}
+                >
+                  <img
+                    src={productItem.thumbnail}
+                    alt={productItem.title}
+                    height={100}
+                    width={100}
+                  />
+                  <span>{productItem.title}</span>
+                </div>
+              );
+            })
+          : null}
+      </div>
+      <div>
+        <button disabled={isButtonDisabled} onClick={() => setCount(count + 1)}>
+          Load More Products
+        </button>
+        {isButtonDisabled ? (
+          <span> You've reached to 100 products </span>
+        ) : null}
+      </div>
     </div>
   );
 }
--- 05_load_more/src/main.jsx
+++ 05_load_more/src/main.jsx
@@ -5,6 +5,6 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App url={`https://dummyjson.com/products`} limit={20} />
+    <App url={`https://dummyjson.com/products`} />
   </StrictMode>,
 );
