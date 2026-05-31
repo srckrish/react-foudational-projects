6f59faba24e08d442b41398403a05a87c3ad73ce
srckrish <krish.dahal82@gmail.com>
2026-05-31 17:41:47 +0545

    style(ui): improve layout

--- 07_QR_code_generator/src/App.jsx
+++ 07_QR_code_generator/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     setQrCodeValue(inputValue);
     setInputValue("");
   }
+  console.log(inputValue);
 
   return (
     <div className="wrapper max-w-2xl mx-auto flex flex-col gap-10 justify-center items-center min-h-screen">
@@ -16,7 +17,10 @@ function App() {
       </h3>
       <div className="flex justify-center items-center gap-10">
         <input
-          onChange={(e) => setInputValue(e.target.value)}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+            setQrCodeValue("");
+          }}
           type="text"
           value={inputValue}
           placeholder="Enter Value"
@@ -24,17 +28,27 @@ function App() {
         />
 
         <button
-          disabled={inputValue && inputValue.trim() !== "" ? false : true}
+          disabled={inputValue.trim() !== "" ? false : true}
           onClick={() => handleGenerateQRCode()}
           className={`bg-cyan-500 text-white font-semibold p-2 rounded-lg cursor-pointer hover:bg-green-700 `}
         >
           Generate
         </button>
       </div>
-      {qrCodeValue && qrCodeValue.trim() !== "" && (
-        <QRCode id="qr-code" value={qrCodeValue} size={400} bgColor="#fff" />
+      {qrCodeValue.trim() !== "" && (
+        <>
+          <QRCode id="qr-code" value={qrCodeValue} size={400} bgColor="#fff" />
+          <h3 className="text-2xl font-semibold text-cyan-800">
+            Your QR Code Is Ready!
+          </h3>
+        </>
       )}
-      <h3 className="text-2xl font-semibold text-cyan-800">{qrCodeValue}</h3>
+
+      {inputValue.trim() === "" && qrCodeValue === "" ? (
+        <h3 className="text-2xl font-semibold text-red-800">
+          Enter a value to generate a QR code.
+        </h3>
+      ) : null}
     </div>
   );
 }
