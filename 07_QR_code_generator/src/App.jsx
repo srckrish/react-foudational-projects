c3c68aa45da4567969be705e6ab566a62ae5ab84
srckrish <krish.dahal82@gmail.com>
2026-05-31 16:52:15 +0545

    feat: implement QR Code generate functionality

--- 07_QR_code_generator/src/App.jsx
+++ 07_QR_code_generator/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { QRCode } from "react-qr-code";
 function App() {
+  const [qrCodeValue, setQrCodeValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  function handleGenerateQRCode() {
+    setQrCodeValue(inputValue);
+    setInputValue("");
+  }
+
   return (
-    <div className='wrapper'>App</div>
-  )
+    <div className="wrapper">
+      <input
+        onChange={(e) => setInputValue(e.target.value)}
+        type="text"
+        value={inputValue}
+        placeholder="Enter Value"
+      />
+
+      <button onClick={() => handleGenerateQRCode()}>Generate</button>
+      <QRCode id="qr-code" value={qrCodeValue} size={400} bgColor="#fff" />
+    </div>
+  );
 }
 
-export default App
+export default App;
