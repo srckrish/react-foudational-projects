662ed13d0b08b8474b4c381ecc7c43aa3217a675
srckrish <krish.dahal82@gmail.com>
2026-06-01 23:58:37 +0545

    get data from the API

--- 09_progress_bar/src/App.jsx
+++ 09_progress_bar/src/App.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
-function App() {
+function App({ url }) {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  async function fetchData(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(getUrl);
+
+      const data = await response.json();
+
+      if (data.products && data.products.length > 0) {
+        console.log(data.products);
+        setProducts(data.products);
+      }
+    } catch (error) {
+      console.log(error);
+      setErrorMessage(error.message);
+    } finally {
+      setLoading(false);
+      console.log("Promise Fulfilled");
+    }
+  }
+
+  useEffect(() => {
+    fetchData(url);
+  }, [url]);
   return <div className="wrapper">App</div>;
 }
 
