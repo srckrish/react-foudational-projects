f9bc3c7dc1740a3ba42d7fb0b0c819bfa648fd3b
srckrish <krish.dahal82@gmail.com>
2026-06-01 14:11:52 +0545

    feat: implement theme mode with local storage hook

--- 08_theme_mode/src/App.jsx
+++ 08_theme_mode/src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import useLocalStorage from "./useLocalStorage";
 
 function App() {
-  return <div className="wrapper">App</div>;
+  const [theme, setTheme] = useLocalStorage("theme", "dark");
+  function changeTheme() {
+    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
+  }
+
+  console.log(theme);
+
+  return (
+    <div
+      className={`wrapper flex items-center min-h-screen ${theme === "dark" ? "bg-black text-white" : ""}`}
+    >
+      <div className="flex flex-col gap-10 items-start mx-10">
+        <h2>Theme Mode: {theme} </h2>
+        <button
+          onClick={() => changeTheme()}
+          className="border rounded-lg px-5 py-3 bg-cyan-700 text-white font-semibold cursor-pointer hover:bg-green-600"
+        >
+          Change Theme
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
