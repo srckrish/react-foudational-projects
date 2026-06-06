3803bc07e92446a8408ca7de891cddf37e7d7113
srckrish <krish.dahal82@gmail.com>
2026-06-06 16:02:26 +0545

    feat: add the progress bar functionality based on page scrolled

--- 09_progress_bar/src/App.jsx
+++ 09_progress_bar/src/App.jsx
@@ -5,7 +5,7 @@ function App({ url }) {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
-
+  const [scrollProgress, setScrollProgress] = useState(0);
   async function fetchData(getUrl) {
     try {
       setLoading(true);
@@ -14,7 +14,7 @@ function App({ url }) {
       const data = await response.json();
 
       if (data.products && data.products.length > 0) {
-        console.log(data.products);
+        // console.log(data.products);
         setProducts(data.products);
       }
     } catch (error) {
@@ -29,7 +29,67 @@ function App({ url }) {
   useEffect(() => {
     fetchData(url);
   }, [url]);
-  return <div className="wrapper">App</div>;
+
+  function handleScrollProgress() {
+    // console.log(
+    //   document.body.scrollTop,
+    //   document.body.scrollHeight,
+    //   document.body.clientHeight,
+    // );
+    // console.log(
+    //   document.documentElement.scrollTop,
+    //   document.documentElement.scrollHeight,
+    //   document.documentElement.clientHeight,
+    // );
+
+    const scrollOnPage =
+      document.body.ScrollTop || document.documentElement.scrollTop;
+
+    const pageHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    setScrollProgress(Math.ceil((scrollOnPage / pageHeight) * 100));
+    // console.log((scrollOnPage / pageHeight) * 100);
+  }
+
+  useEffect(() => {
+    function handleScroll() {
+      handleScrollProgress();
+    }
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  if (loading) {
+    return <div>Loading data....</div>;
+  }
+
+  if (errorMessage) {
+    return <div>{errorMessage}</div>;
+  }
+
+  return (
+    <div className="wrapper">
+      <div className={`progress-bar sticky top-0 bg-gray-300 w-full h-6`}>
+        <div
+          className={`current-progress bg-green-600 h-6 flex items-center justify-end text-white text-xs rounded-r-lg ${scrollProgress === 0 ? "" : "pr-2"}`}
+          style={{ width: `${scrollProgress}%` }}
+        >
+          {Math.ceil(scrollProgress)}%
+        </div>
+      </div>
+
+      {products && products.length > 0
+        ? products.map((productItem) => (
+            <h3 key={productItem.id}> {productItem.title} </h3>
+          ))
+        : "No data found"}
+    </div>
+  );
 }
 
 export default App;
