e07d0c70903ce57d5a77f557ade7af44eafb2793
srckrish <krish.dahal82@gmail.com>
2026-06-12 23:59:30 +0545

    add styling

--- 12_github_profile_finder/src/App.jsx
+++ 12_github_profile_finder/src/App.jsx
@@ -48,9 +48,9 @@ function App() {
 
   return (
     <>
-      <div className="wrapper h-screen w-full flex flex-col gap- 5 items-center justify-center">
-        <div className="container text-center h-9/12 rounded-lg flex flex-col items-center">
-          <div className="username-input">
+      <div className="wrapper h-screen w-full flex flex-col gap- 5 items-center justify-center text-white bg-gray-900">
+        <div className="container text-center h-9/12 rounded-lg flex flex-col items-center bg-gray-800">
+          <div className="username-input text-2xl font-semibold flex gap-5 items-center justify-center mt-10 mb-10">
             <input
               type="text"
               name="username"
@@ -58,28 +58,37 @@ function App() {
               autoComplete="true"
               onChange={(e) => setInput(e.target.value)}
               value={input}
+              className="text-white bg-gray-800 border border-gray-700 rounded-lg p-2"
             />
-            <button onClick={() => handleLoginValue()}>Search</button>
-
-            {data?.id && (
-              <div className="user-data">
-                <div className="user-image">
-                  <img src={data?.avatar_url} />
-                </div>
-                <div className="user-name">Name: {data?.name}</div>
-                <div className="user-repos">
-                  Public Repos: {data?.public_repos}
-                </div>
-                <div className="user-followers">
-                  Followers: {data?.followers}
-                </div>
-                <div className="user-following ">
-                  Following: {data?.following}
-                </div>
-                <div className="user-joined">{data?.created_at}</div>
-              </div>
-            )}
+            <button
+              onClick={() => handleLoginValue()}
+              className="text-white bg-gray-800 border border-gray-700 rounded-lg p-2 font-semibold"
+            >
+              Search
+            </button>
           </div>
+          {data?.id && (
+            <div className="user-data text-white flex flex-col gap-5 items-center mt-10">
+              <div className="user-image text-white w-24 h-24 rounded-full">
+                <img src={data?.avatar_url} />
+              </div>
+              <div className="user-name text-2xl font-semibold">
+                Name: {data?.name}
+              </div>
+              <div className="user-repos text-white text-xl font-semibold text-left">
+                Public Repos: {data?.public_repos}
+              </div>
+              <div className="user-followers text-white text-xl font-semibold text-left">
+                Followers: {data?.followers}
+              </div>
+              <div className="user-following text-white text-xl font-semibold">
+                Following: {data?.following}
+              </div>
+              <div className="user-joined text-white text-xl font-semibold">
+                {data?.created_at}
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
