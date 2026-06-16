bb8d1f9c9b09d2b497bb3c51a5387b326f3f2d69
srckrish <krish.dahal82@gmail.com>
2026-06-16 14:21:12 +0545

    complete implementation of useFetch hook

--- 16_useFetch_custom_hook/src/App.jsx
+++ 16_useFetch_custom_hook/src/App.jsx
@@ -16,10 +16,12 @@ function App() {
   }
 
   return (
-    <div className="wrapper bg-red-500">
+    <div className="wrapper p-4">
       {data && data.products && data.products.length > 0
         ? data.products.map((dataItem) => (
-            <h3 key={dataItem.id}>{dataItem.title}</h3>
+            <h3 key={dataItem.id} className="text-lg font-mono">
+              {dataItem.title}
+            </h3>
           ))
         : "No data found"}
     </div>
