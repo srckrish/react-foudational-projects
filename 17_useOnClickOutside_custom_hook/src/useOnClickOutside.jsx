a819a5205e27a50513e17db8e70179d47ea72600
srckrish <krish.dahal82@gmail.com>
2026-06-20 16:35:44 +0545

    fix: properly clean up event listeners in useOnClickOutside

--- 17_useOnClickOutside_custom_hook/src/useOnClickOutside.jsx
+++ 17_useOnClickOutside_custom_hook/src/useOnClickOutside.jsx
@@ -1,17 +1,16 @@
 import React, { useEffect } from "react";
 
 function useOnClickOutside(ref, handler) {
-  function listener(e) {
-    if (!ref.current || ref.current.contains(e.target)) return;
-    handler(e);
-  }
-
   useEffect(() => {
+    function listener(e) {
+      if (!ref.current || ref.current.contains(e.target)) return;
+      handler(e);
+    }
     document.addEventListener("mousedown", listener);
     document.addEventListener("touchstart", listener);
 
     return () => {
-      document.removeEventListener("mousedown", () => listener());
+      document.removeEventListener("mousedown", listener);
       document.removeEventListener("touchstart", listener);
     };
   }, [ref, handler]);
